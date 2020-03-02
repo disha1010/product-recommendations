a484ba0dde1530413a36241d4e6c57b3521d5bac
dish1010 <>
2020-03-02 18:07:49 +0300

    initial commit: install bootstrap, loaders and plugins

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: "./dist"
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins: function () { // postcss plugins, can be exported to postcss.config.js
+              return [
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin("style.css"),
+    new HtmlWebpackPlugin({template: "./src/index.html"}),
+  ],
+};
