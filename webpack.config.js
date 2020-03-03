f1dd2700912d7763e3f1285c16fed84dffcd6dc5
disha1010 <diana.saginbaeva@gmail.com>
2020-03-03 20:53:01 +0300

    edit config files

--- webpack.config.js
+++ webpack.config.js
@@ -34,11 +34,28 @@ module.exports = {
       },
       { 
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+        use: [
+          {
+            loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+        
       },
       { 
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: 'file-loader' 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ] 
       }
     ]
   },
